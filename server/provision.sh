2e03e05a260e7f8d73e1937a06f0aa12d3baa107
Bracken King - LACRM <bracken@lessannoyingcrm.com>
2018-10-13 16:34:11 -0500

    dev env provision tweak

--- server/provision.sh
+++ server/provision.sh
@@ -8,7 +8,7 @@ echo "Installing Git..."
 sudo apt-get install git -y > /dev/null
 sudo add-apt-repository ppa:ondrej/php
 sudo apt-get update
-sudo apt-get install -y emacs git mysql-client nginx php7.0-fpm php7.0-mysql php7.0-mbstring php7.0-mcrypt php7.0-curl php7.0-zip php7.0-bcmath s3cmd awscli ssl-cert
+sudo apt-get install -y emacs git mysql-client nginx php7.0-fpm php7.0-mysql php7.0-mbstring php7.0-mcrypt php7.0-curl php7.0-zip php7.0-bcmath php7.0-xml s3cmd awscli ssl-cert
 #echo "Installing Node and NVM..."
 #curl -o- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 #source ~/.nvm/nvm.sh
