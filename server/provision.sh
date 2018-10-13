893080e4e9ccc4c1a3071fc3763b57e9d971365d
Bracken King - LACRM <bracken@lessannoyingcrm.com>
2018-10-13 00:35:45 -0500

    no need node

--- server/provision.sh
+++ server/provision.sh
@@ -9,11 +9,11 @@ sudo apt-get install git -y > /dev/null
 sudo add-apt-repository ppa:ondrej/php
 sudo apt-get update
 sudo apt-get install -y emacs git mysql-client nginx php7.0-fpm php7.0-mysql php7.0-mbstring php7.0-mcrypt php7.0-curl php7.0-zip php7.0-bcmath s3cmd awscli ssl-cert
-echo "Installing Node and NVM..."
-curl -o- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
-source ~/.nvm/nvm.sh
-nvm install node
-nvm alias default node
+#echo "Installing Node and NVM..."
+#curl -o- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+#source ~/.nvm/nvm.sh
+#nvm install node
+#nvm alias default node
 
 sudo openssl dhparam -dsaparam -out /etc/ssl/certs/dhparam.pem 4096
 sudo cp /vagrant/server/api.globalhacrm.com /etc/nginx/sites-available/
