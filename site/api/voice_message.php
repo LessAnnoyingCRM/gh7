d47934fd0fa1bed23c6336fb385bcb05905cfde0
meagles <mitch.d.eagles@gmail.com>
2018-10-13 02:26:03 -0500

    voice message functions

---
+++ site/api/voice_message.php
@@ -0,0 +1,16 @@
+<?php
+
+function InsertVoiceMessage ($Parameters) {
+	$Keys = array_keys($Parameters);
+	$Values = array_values($Parameters);
+	$Sql = "INSERT INTO user ($Keys) VALUES ($Values)";
+	mysqli_query($Conn, $Sql);
+}
+
+function GetVoiceMessages ($MatchId) {
+	$Sql = "SELECT * FROM message WHERE MatchId = $MatchId";
+	mysqli_query($Conn, $Sql);
+}
+
+
+
