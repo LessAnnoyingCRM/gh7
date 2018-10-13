f855ccc6e3ee7f8004f52b341b3427ddc665c0cd
meagles <mitch.d.eagles@gmail.com>
2018-10-13 14:37:58 -0500

    event functions

---
+++ site/api/event.php
@@ -0,0 +1,37 @@
+<?php
+
+function CreateEvent($Parameters) {
+	$MatchId = $Parameters['MatchId'];
+	$Location = $Parameters['Location'];
+	$EventDate = $Parameters['EventDate'];
+
+	$Sql = "INSERT INTO event (MatchId, Location, EventDate)
+			VALUES ($MatchId, $Location, $EventDate)";
+
+	try {
+		mysqli_query($Conn, $Sql);
+		$EventId = mysqli_insert_id($Conn);
+	} catch (Exception $e) {
+		error_log($e->getMessage());
+		return "Error";
+	}
+
+	return array("EventId" => $EventId);
+}
+
+function ConfirmEvent($Parameters) {
+	$EventId = $Parameters['EventId'];
+	$WhichUser_DateConfirmed = ($Parameters['IsHost'] ? "DateHostConfirmed" : "DateGuestConfirmed");
+	$Sql = "UPDATE event 
+			SET WhichUser_DateConfirmed = '$DateConfirmed'
+			WHERE EventId = $EventId";
+
+	try {
+		mysqli_query($Conn, $Sql);
+	} catch (Exception $e) {
+		error_log($e->getMessage());
+		return "Error";
+	}
+
+	return array();
+}
