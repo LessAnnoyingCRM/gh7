929d9b4854d110bb5e6f64e66c3166990afffe7b
Bracken King - LACRM <bracken@lessannoyingcrm.com>
2018-10-13 18:42:38 -0500

    updates for testing s3

--- site/business_logic/test.php
+++ site/business_logic/test.php
@@ -7,7 +7,7 @@ function TestHelloWorld() {
 function TestS3() {
     $S3Filename = 'test.mp4';
     $LocalFilename = '/tmp/Sample.mp3';
-    $Url = UploadMp4S3($S3Filename,$LocalFilename);
+    $Url = _UploadMp4S3($S3Filename,$LocalFilename);
 
     return(array('FileUrl'=>$Url));
 }
--- site/init.php
+++ site/init.php
@@ -12,7 +12,10 @@ include_once('./mysql_framework.php');
 include_once('../business_logic/lib/aws.phar');
 
 //business logic
+include_once('../business_logic/event.php');
+include_once('../business_logic/match.php');
+include_once('../business_logic/rating.php');
 include_once('../business_logic/test.php');
 include_once('../business_logic/user.php');
-include_once('../business_logic/match.php');
+include_once('../business_logic/voice_message.php');
 
