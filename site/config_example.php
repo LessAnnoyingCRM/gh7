f0c9a73ff530b390013f1a84b51d4e7bed431298
Bracken King - LACRM <bracken@lessannoyingcrm.com>
2018-10-13 18:33:48 -0500

    example config file

---
+++ site/config_example.php
@@ -0,0 +1,16 @@
+<?php
+
+//PHP settings (only on dev)
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+define('AWS_KEY','');
+define('AWS_SECRET','');
+
+define('DB_HOST','192.168.33.16');
+define('DB_USER','gh7');
+define('DB_PASSWORD','globalhacrm');
+define('DB_DATABASE','gh7');
+
