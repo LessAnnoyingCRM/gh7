d240fb93423fe556c9095462ced8452e5f4ce452
Bracken King - LACRM <bracken@lessannoyingcrm.com>
2018-10-13 12:56:44 -0500

    create DB

---
+++ server/createDb.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+sudo apt-get install mysql-server
+#globalhacrm
+
+#echo edited /etc/mysql/my.cnf to change bind-address to 0.0.0.0 (to enable remote access)
+#CREATE USER gh7@'%' IDENTIFIED BY 'globalhacrm';
+#GRANT ALL PRIVILEGES ON *.* TO 'gh7'@'%';
+#FLUSH PRIVILEGES;
+
