3b85cd5030c772fe621950ea0b554675b760b7fa
Bracken King - LACRM <bracken@lessannoyingcrm.com>
2018-10-13 20:14:37 -0500

    save files in the dummy endpoint

--- site/api/api_endpoint_dummy.php
+++ site/api/api_endpoint_dummy.php
@@ -8,6 +8,13 @@ $OutputText .= print_r($_FILES,true)."\n\n";
 $OutputText .= '$_HTTP_POST_FILES:'."\n";
 $OutputText .= print_r($HTTP_POST_FILES,true)."\n\n";
 
+if ($_FILES) {
+    foreach	($_FILES as $File) {
+        echo('saving '.$File['tmp_name']);
+        `cp $File[tmp_name] /tmp/save`;
+    }
+}
+
 $out = '/tmp/'.round(microtime(true) * 1000);
 $file = fopen($out,'w');
 fwrite($file, $OutputText);
