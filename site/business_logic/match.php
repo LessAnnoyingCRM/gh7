977403cc2df81b9b40d3a50b00888fc78df39116
meagles <mitch.d.eagles@gmail.com>
2018-10-14 00:53:25 -0500

    get potential matches

--- site/business_logic/match.php
+++ site/business_logic/match.php
@@ -6,15 +6,21 @@ function CreateMatch ($Parameters, $UserId) {
 	return Mysql_GetLastCreatedId("MatchId");
 }
 
-function GetPotentialMatches($Parameters, $UserId) {
+function GetPotentialMatches($Parameters, $UserId, $IsHost) {
 
     $User = _GetUser($UserId);
 
     if ($User['IsHost']) {
-        $Sql = "SELECT UserId FROM user WHERE !IsHost";
+        $Sql = "SELECT user.UserId AS OtherUserId, user.Name, user.ProfilePicture
+        		FROM pairing
+        		LEFT JOIN ON user WHERE user.UserId = pairing.GuestId 
+        		WHERE DateGuestApproved IS NOT NULL AND DateUnmatched IS NULL AND pairing.HostId = $UserId";
     }
     else {
-        $Sql = "SELECT UserId FROM user WHERE IsHost";
+        $Sql = "SELECT user.UserId AS OtherUserId, user.Name, user.
+        		FROM pairing 
+        		LEFT JOIN ON user WHERE user.UserId = pairing.HostId
+        		WHERE DateHostMatched IS NULL AND DateUnmatched IS NULL AND pairing.GuestId = $UserId";
     }
 
     $Result = Mysqlx_Query($Sql);
@@ -30,6 +36,16 @@ function GetMatches ($Parameters, $UserId, $IsHost) {
 	
 	$Result = Mysqlx_Query($Sql);
 	return Mysql_GetAssocArray($Result);
+}
+
+function HandleResponse ($Parameters, $UserId, $IsHost) {
+
+	$OtherUserId = $Parameters['OtherUserId'];
+	$ThisUserTypeId = ($IsHost ? $Parameters)
+
+	$Sql = "UPDATE pairing 
+			LEFT JOIN ON pairing.MatchId = "
+
 }
 
 function GuestApproveMatch ($Parameters) {
