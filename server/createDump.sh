7f34ffbc9e7186337916303c8cb6d9b23c297bd4
Bracken King - LACRM <bracken@lessannoyingcrm.com>
2018-10-13 20:30:15 -0500

    new dumps

---
+++ server/createDump.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mysqldump -u gh7 -pglobalhacrm gh7 >dump.sql
--- server/dump.sql
+++ server/dump.sql
@@ -1,30 +1,27 @@
-# ************************************************************
-# Sequel Pro SQL dump
-# Version 4541
-#
-# http://www.sequelpro.com/
-# https://github.com/sequelpro/sequelpro
-#
-# Host: 192.168.33.16 (MySQL 5.5.61-0ubuntu0.14.04.1)
-# Database: local
-# Generation Time: 2018-10-13 23:03:27 +0000
-# ************************************************************
-
+-- MySQL dump 10.13  Distrib 5.5.61, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: gh7
+-- ------------------------------------------------------
+-- Server version	5.5.61-0ubuntu0.14.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-
-# Dump of table event
-# ------------------------------------------------------------
+--
+-- Table structure for table `event`
+--
 
 DROP TABLE IF EXISTS `event`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `event` (
   `EventId` int(11) NOT NULL AUTO_INCREMENT,
   `MatchId` int(11) NOT NULL,
@@ -35,48 +32,79 @@ CREATE TABLE `event` (
   `EventHappened` tinyint(4) DEFAULT NULL,
   PRIMARY KEY (`EventId`),
   KEY `MatchId` (`MatchId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `event`
+--
 
 LOCK TABLES `event` WRITE;
 /*!40000 ALTER TABLE `event` DISABLE KEYS */;
-
-INSERT INTO `event` (`EventId`, `MatchId`, `Location`, `EventDate`, `DateHostConfirmed`, `DateGuestConfirmed`, `EventHappened`)
-VALUES
-	(1,1,'3325 Brookview Dr','2018-10-16',NULL,NULL,NULL);
-
+INSERT INTO `event` VALUES (1,1,'3325 Brookview Dr','2018-10-16',NULL,NULL,NULL);
 /*!40000 ALTER TABLE `event` ENABLE KEYS */;
 UNLOCK TABLES;
 
-
-# Dump of table language
-# ------------------------------------------------------------
+--
+-- Table structure for table `language`
+--
 
 DROP TABLE IF EXISTS `language`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `language` (
   `LanguageId` int(11) NOT NULL AUTO_INCREMENT,
   `LanguageName` varchar(50) NOT NULL,
   PRIMARY KEY (`LanguageId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `language`
+--
 
 LOCK TABLES `language` WRITE;
 /*!40000 ALTER TABLE `language` DISABLE KEYS */;
-
-INSERT INTO `language` (`LanguageId`, `LanguageName`)
-VALUES
-	(1,'English'),
-	(2,'Español'),
-	(3,'Русский');
-
+INSERT INTO `language` VALUES (1,'English'),(2,'Español'),(3,'Русский');
 /*!40000 ALTER TABLE `language` ENABLE KEYS */;
 UNLOCK TABLES;
 
+--
+-- Table structure for table `message`
+--
 
-# Dump of table pairing
-# ------------------------------------------------------------
+DROP TABLE IF EXISTS `message`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `message` (
+  `MessageId` int(11) NOT NULL AUTO_INCREMENT,
+  `SendingUserId` int(11) NOT NULL,
+  `MatchId` int(11) NOT NULL,
+  `DateSent` datetime DEFAULT NULL,
+  `MessageUrl` varchar(250) NOT NULL DEFAULT '',
+  `DateArchived` datetime DEFAULT NULL,
+  PRIMARY KEY (`MessageId`),
+  KEY `UserId` (`SendingUserId`),
+  KEY `MatchId` (`MatchId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `message`
+--
+
+LOCK TABLES `message` WRITE;
+/*!40000 ALTER TABLE `message` DISABLE KEYS */;
+/*!40000 ALTER TABLE `message` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pairing`
+--
 
 DROP TABLE IF EXISTS `pairing`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `pairing` (
   `MatchId` int(11) NOT NULL AUTO_INCREMENT,
   `GuestId` int(11) NOT NULL,
@@ -86,44 +114,26 @@ CREATE TABLE `pairing` (
   `DateUnmatched` datetime DEFAULT NULL,
   `DateLastPresented` datetime DEFAULT NULL,
   PRIMARY KEY (`MatchId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pairing`
+--
 
 LOCK TABLES `pairing` WRITE;
 /*!40000 ALTER TABLE `pairing` DISABLE KEYS */;
-
-INSERT INTO `pairing` (`MatchId`, `GuestId`, `HostId`, `DateGuestApproved`, `DateHostMatched`, `DateUnmatched`, `DateLastPresented`)
-VALUES
-	(2,1,2,NULL,NULL,NULL,NULL),
-	(3,1,2,NULL,NULL,NULL,NULL);
-
+INSERT INTO `pairing` VALUES (2,1,2,NULL,NULL,NULL,NULL),(3,1,2,NULL,NULL,NULL,NULL);
 /*!40000 ALTER TABLE `pairing` ENABLE KEYS */;
 UNLOCK TABLES;
 
-
-# Dump of table message
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `message`;
-
-CREATE TABLE `message` (
-  `MessageId` int(11) NOT NULL AUTO_INCREMENT,
-  `SendingUserId` int(11) NOT NULL,
-  `MatchId` int(11) NOT NULL,
-  `DateSent` datetime DEFAULT NULL,
-  `RecordingFile` varchar(250) NOT NULL,
-  `DateArchived` datetime DEFAULT NULL,
-  PRIMARY KEY (`MessageId`),
-  KEY `UserId` (`SendingUserId`),
-  KEY `MatchId` (`MatchId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-
-# Dump of table rating
-# ------------------------------------------------------------
+--
+-- Table structure for table `rating`
+--
 
 DROP TABLE IF EXISTS `rating`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `rating` (
   `EventId` int(11) NOT NULL,
   `RatingUser` int(11) NOT NULL,
@@ -136,14 +146,24 @@ CREATE TABLE `rating` (
   KEY `RatingUser` (`RatingUser`),
   KEY `RatedUser` (`RatedUser`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `rating`
+--
 
+LOCK TABLES `rating` WRITE;
+/*!40000 ALTER TABLE `rating` DISABLE KEYS */;
+/*!40000 ALTER TABLE `rating` ENABLE KEYS */;
+UNLOCK TABLES;
 
-# Dump of table user
-# ------------------------------------------------------------
+--
+-- Table structure for table `user`
+--
 
 DROP TABLE IF EXISTS `user`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
   `UserId` int(11) NOT NULL AUTO_INCREMENT,
   `Name` varchar(250) NOT NULL,
@@ -155,50 +175,51 @@ CREATE TABLE `user` (
   `Picture` varchar(250) DEFAULT NULL,
   `IsHost` tinyint(1) NOT NULL,
   PRIMARY KEY (`UserId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-
-INSERT INTO `user` (`UserId`, `Name`, `Email`, `Password`, `DateCreated`, `DateArchived`, `Profile`, `Picture`, `IsHost`)
-VALUES
-	(1,'Mitch Eagles','mitch.eagles@lessannoyingcrm.com','$2a$92$VL5I8lFuvLUfTteV2S2h5uJRdbepSbm5PiCzBceFUP8J42q/s/wxq','2018-10-13 01:17:53',NULL,NULL,NULL,1),
-	(2,'Olga K','olya@gmail.com','$2a$92$VL5I8lFuvLUfTteV2S2h5uJRdbepSbm5PiCzBceFUP8J42q/s/wxq','2018-10-13 01:56:29',NULL,NULL,NULL,0);
-
+INSERT INTO `user` VALUES (1,'Mitch Eagles','mitch.eagles@lessannoyingcrm.com','$2a$92$VL5I8lFuvLUfTteV2S2h5uJRdbepSbm5PiCzBceFUP8J42q/s/wxq','2018-10-13 01:17:53',NULL,NULL,NULL,1),(2,'Olga K','olya@gmail.com','$2a$92$VL5I8lFuvLUfTteV2S2h5uJRdbepSbm5PiCzBceFUP8J42q/s/wxq','2018-10-13 01:56:29',NULL,NULL,NULL,0);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 
-
-# Dump of table user_language
-# ------------------------------------------------------------
+--
+-- Table structure for table `user_language`
+--
 
 DROP TABLE IF EXISTS `user_language`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user_language` (
   `UserId` int(11) NOT NULL,
   `LanguageId` int(11) NOT NULL,
   `Ranking` int(11) NOT NULL COMMENT 'Ranking from 1-3',
   KEY `UserId` (`UserId`,`LanguageId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_language`
+--
 
 LOCK TABLES `user_language` WRITE;
 /*!40000 ALTER TABLE `user_language` DISABLE KEYS */;
-
-INSERT INTO `user_language` (`UserId`, `LanguageId`, `Ranking`)
-VALUES
-	(1,1,3),
-	(1,2,1),
-	(2,1,2),
-	(2,3,3);
-
+INSERT INTO `user_language` VALUES (1,1,3),(1,2,1),(2,1,2),(2,3,3);
 /*!40000 ALTER TABLE `user_language` ENABLE KEYS */;
 UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-
-
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-10-14  1:24:11
