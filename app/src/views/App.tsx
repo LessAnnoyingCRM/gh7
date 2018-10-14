612ee0edbb5d7691f4ba6b1e96fec7a2b90a1da8
robertlandis <robertwlandis@gmail.com>
2018-10-13 23:03:01 -0500

    this got borked

--- app/src/views/App.tsx
+++ app/src/views/App.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
+import { Provider } from 'mobx-react';
 import Router from './Router';
+import Stores from '../stores/index';
 
 type Props = {}
 export default class App extends React.Component<Props> {
   render() {
     return (
-      <Router />
+        <Provider {...Stores}>
+          <Router />
+        </Provider>
     );
   }
 }
